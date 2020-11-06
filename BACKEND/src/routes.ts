e644f87ed150e8177f8391c5ab469a4e33dc0a6a
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2020-11-06 20:50:04 -0300

    up

--- BACKEND/src/routes.ts
+++ BACKEND/src/routes.ts
@@ -3,6 +3,7 @@ import OrphanagesControllers from './controllers/Orphanagescontrollers';
 
 const routes = Router();
 routes.get('/orphanages', OrphanagesControllers.index);
+routes.get('/orphanages/:id', OrphanagesControllers.show);
 routes.post('/orphanages', OrphanagesControllers.create);
 
 
