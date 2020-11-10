109b04feee781f99999e6383830b99a10066eb7a
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2020-11-10 11:11:59 -0300

    up

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/Uploads' , express.static(path.join(__dirname,'..', 'Uploads')));
+app.use('/uploads' , express.static(path.join(__dirname, '../', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
